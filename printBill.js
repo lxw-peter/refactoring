47dc55920b4dc6184bff0598d87c4541e84fa5f6
lxw-peter <1270542959@qq.com>
2019-04-11 22:12:15 +0800

    开始为添加新功能做准备

--- printBill.js
+++ printBill.js
@@ -5,76 +5,82 @@ const playsPath = './json/plays.json'
 const invoice = JSON.parse(fs.readFileSync(invoicesPath, 'utf-8'))
 const plays = JSON.parse(fs.readFileSync(playsPath, 'utf-8'))
 
-function statement (invoice, plays) {
-  let totalAmount = 0
-  let result = `Statement for ${invoice.customer}\n`
-  for (let perf of invoice.performances) {
+function statement(invoice, plays) {
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances
+  return renderPlainText(statementData, plays)
+}
+
+function renderPlainText (data, plays) {
+  let result = `Statement for ${data.customer}\n`
+  for (let perf of data.performances) {
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience} seats)\n`
   }
-  result += `Amount owed is ${usd(totalAmountFor())}\n`
+  result += `Amount owed is ${usd(totalAmount())}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
   return result
-}
 
-// 将函数返回值统一为result
-// 修改变量名 perf => aPerformance 不定冠词 a 表示数组array
-function amountFor(aPerformance) {
-  let result = 0
-  switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20)
-      }
-      result += 300 * aPerformance.audience
-      break
-    default:
-      throw new Error(`unknown type: ${play.type}`)
+  // 将函数返回值统一为result
+  // 修改变量名 perf => aPerformance 不定冠词 a 表示数组array
+  function amountFor(aPerformance) {
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unknown type: ${play.type}`)
+    }
+    return result
   }
-  return result
-}
 
-function totalAmountFor() {
-  let totalAmount = 0
-  for (let perf of invoice.performances) {
-    totalAmount += amountFor(perf)
+  function totalAmount() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += amountFor(perf)
+    }
+    return result
   }
-  return totalAmount
-}
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID]
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0
-  result += Math.max(aPerformance.audience - 30, 0)
-  if (playFor(aPerformance).type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-  return result
-}
-
-function totalVolumeCredits() {
-  let result = 0
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf)
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
   }
-  return result
-}
 
-function usd(aNumber) {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 2
-  }).format(aNumber / 100)
-}
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if (playFor(aPerformance).type === 'comedy') result += Math.floor(aPerformance.audience / 5)
+    return result
+  }
 
+  function totalVolumeCredits() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf)
+    }
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 2
+    }).format(aNumber / 100)
+  }
+
+}
 
 
 module.exports = {
