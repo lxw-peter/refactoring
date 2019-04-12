e02cd042934e7cabde40e44759237b36e277be41
lxw-peter <1270542959@qq.com>
2019-04-12 23:29:45 +0800

    浣跨敤澶氭€佽绠楀櫒鎻愪緵鏁版嵁

--- createStatementData.js
+++ createStatementData.js
@@ -1,17 +1,66 @@
-function createStatementData(invoice, plays) {
+// performance: 表演
+class PerformanceCalculator {
+  constructor( aPerformance, aPlay ) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+  
+  get amount() {
+    throw new Error('subclass responsibility')
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
+
+
+class TragedyCalculator extends PerformanceCalculator{
+  get amount() {
+    let result = 40000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result
+  }
+}
+class ComedyCalculator extends PerformanceCalculator{
+  get amount() {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor( this.performance.audience / 5 )
+  }
+}
+
+function createPerformanceCalculator( aPerformance, aPlay ) {
+  switch(aPlay.type) {
+    case "tragedy": return new TragedyCalculator( aPerformance, aPlay ) 
+    case "comedy": return new ComedyCalculator( aPerformance, aPlay ) 
+    default:
+      throw new Error(`unknown type: ${ aPlay.type }`)
+  }
+}
+
+function createStatementData( invoice, plays ) {
   const statementData = {}
   statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerformance)
-  statementData.totalAmount = totalAmount(statementData)
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+  statementData.performances = invoice.performances.map( enrichPerformance )
+  statementData.totalAmount = totalAmount( statementData )
+  statementData.totalVolumeCredits = totalVolumeCredits( statementData )
   return statementData
   
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
-  
+  function enrichPerformance( aPerformance ) {
+    const calculator = createPerformanceCalculator( aPerformance, playFor(aPerformance) )
+    const result = Object.assign( {}, aPerformance )
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
   
@@ -19,55 +68,15 @@ function createStatementData(invoice, plays) {
     return plays[aPerformance.playID]
   }
   
-  function amountFor(aPerformance) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
-    return result
-  }
-  
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-    return result
-  }
-  
   function totalAmount(data) {
-    // let result = 0
-    // for (let perf of data.performances) {
-    //   result += perf.amount
-    // }
-    // return result;
     return data.performances
     .reduce((total, perf) => total + perf.amount, 0)
   }
   
   function totalVolumeCredits(data) {
-    // let result = 0
-    // for (let perf of data.performances) {
-    //   result += perf.volumeCredits
-    // }
-    // return result
     return data.performances
       .reduce((total, perf) => total + perf.volumeCredits, 0)
   }
 }
-
 
 module.exports = createStatementData
--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "功能完成"
+git commit -m "使用多态计算器提供数据"
 git push
